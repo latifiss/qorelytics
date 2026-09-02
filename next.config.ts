947972b6a9ca8c6992202cc168a0fc9a81c785bf
Latif Issaka <83374530+latifiss@users.noreply.github.com>
2026-09-02 09:23:46 +0000

    chore: ignore TypeScript errors during builds

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   images: {
     remotePatterns: [
       {
